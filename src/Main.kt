a2e892406197080efaed3bb92ca49160fa1dc64f
Admin <reteamboss@gmail.com>
2024-09-10 19:09:22 +0300

    2

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,15 @@
 fun main() {
-    println("Hello World!")
+
+    println(divideIfWhole(100,11))
+    var result = divideIfWhole(100,10) ?: 0
+    println(result)
+}
+
+fun divideIfWhole(value: Int, divisor: Int): Int?{
+    return if (value % divisor == 0) {
+        value / divisor
+    } else {
+        null
+    }
+
 }
